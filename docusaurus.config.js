66be2e12810f6cf33e544c823f38fb514cd6121e
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-10-07 17:32:04 +0200

    add plugin-ideal-image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,12 +13,12 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/weaviate-logo.svg',
-
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'semi-technologies', // Usually your GitHub org/user name.
   projectName: 'weaviate', // Usually your repo name.
-
+  
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -26,7 +26,7 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  
   presets: [
     [
       'classic',
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -52,90 +52,103 @@ const config = {
       }),
     ],
   ],
-
+  
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Weaviate',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/weaviate-logo.svg',
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+    navbar: {
+      title: 'Weaviate',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/weaviate-logo.svg',
+      },
+      items: [
+        {
+          label: 'Docs',
+          sidebarId: 'docsSidebar',
+          type: 'doc',
+          docId: 'index',
+          position: 'left',
         },
-        items: [
-          {
-            label: 'Docs',
-            sidebarId: 'docsSidebar',
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-          },
-          {
-            label: 'Contributor Guide',
-            sidebarId: 'contributorSidebar',
-            type: 'doc',
-            docId: 'contributor-guide/index',
-            // docId: 'contributor-guide/intro',
-            position: 'left',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/semi-technologies/weaviate-io',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+        {
+          label: 'Contributor Guide',
+          sidebarId: 'contributorSidebar',
+          type: 'doc',
+          docId: 'contributor-guide/index',
+          // docId: 'contributor-guide/intro',
+          position: 'left',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/semi-technologies/weaviate-io',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/tags/weaviate/',
+            },
+            {
+              label: 'Slack',
+              href: 'https://weaviate.slack.com/',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/weaviate_io',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/semi-technologies/weaviate-io',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Weaviate, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  }),
+  
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/tags/weaviate/',
-              },
-              {
-                label: 'Slack',
-                href: 'https://weaviate.slack.com/',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/weaviate_io',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/semi-technologies/weaviate-io',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Weaviate, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+    ],
+  ],
 };
 
 module.exports = config;
