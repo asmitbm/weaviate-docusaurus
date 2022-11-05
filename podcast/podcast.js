64e3a4d7530a2b55fa8bebdc45df43b43907f7ca
Asmit2952 <asmitbm2952002@gmail.com>
2022-11-05 21:27:13 +0530

    add podcast data

---
+++ podcast/podcast.js
@@ -0,0 +1,52 @@
+var item = [
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-27.png",
+        is_use_case: false
+    },
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-26.png",
+        is_use_case: false
+    },
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-27.png",
+        is_use_case: false
+    },
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-27.png",
+        is_use_case: false
+    },
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-27.png",
+        is_use_case: false
+    },
+    {
+        title: "Neural Magic in Weaviate",
+        description: "Michael Goin explains how Neural Magic uses Sparsity to achieve faster and cheaper Deep Learning inference on CPUs!",
+        youtube: "leGgjIQkVYo",
+        date: "26 October, 2022",
+        cover_image: "img/podcast/podcast-27.png",
+        is_use_case: false
+    }
+]
+
+export default item
