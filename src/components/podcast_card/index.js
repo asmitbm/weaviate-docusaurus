9533efb148d33916b505f93536155057f7671e72
Asmit2952 <asmitbm2952002@gmail.com>
2022-11-17 20:37:16 +0530

    update podcast page

--- src/components/podcast_card/index.js
+++ src/components/podcast_card/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import style from "../../css/podcast/podcast_card.module.css"
 
 export default function PodcastCard(props) {
-    
+
     return (
         <div class={style.podcast_card}>
             <a href={`https://www.youtube.com/watch?v=${props.youtube}`} target="_blank">
-            <img src={props.cover_image} />
-            <div>
-                <h2>{props.title}</h2>
-                <p>{props.description}</p>
-                <a href="#" class="read-more">{props.date}</a>
-            </div>
+                <img src={props.cover_image} />
+                <div>
+                    <h2>{props.title}</h2>
+                    <p>{props.description}</p>
+                    <p>{props.date}</p>
+                </div>
             </a>
         </div>
     )
